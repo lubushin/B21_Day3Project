f5819219dbd9cbbf63230ac19566b577ff602844
Lyubov Ubushin <Lyubau1124@gmail.com>
2020-11-12 10:46:51 -0500

    final work is done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git Last Day!");
         //b21 is exited
+        //B21 are successful testers
+        //B21 is dedicated
 
     }
 }
